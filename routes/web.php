2996975f5e964d8dc6e03fc3b96ac37ea83ef1e7
ronykader <rony.ksr06@gmail.com>
2024-02-03 22:40:24 +0600

    update - web route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    return "Welcome to our Class";
+    return "Welcome to Wallet Master Laravel";
 
 //    return view('welcome');
 });
